babbdb81a233efa7205878373495ad0334bf19b0
hyojoo <jhj080802@naver.com>
2024-04-03 21:20:09 +0900

    evnet

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-const title = document.querySelectorAll(".Hello h1");
-console.log(title);
+// const titke = document.querySelector("div.hello:first =<child h1");
+// function handleTitleClick(){
+//     handleTitleClick.style.color = "blue";
+// }
+
+// function handleMouseEnter(){
+//     handleTitleClick.innerText = "Mouse is here!";
+// }
+
+// function  handleMouseLeave(){
+//     handleTitleClick.innerText = "Mouse is gone!";
+// }
+
+// title.addEventListener("click", handleTitleClick);
+// title.addEventListener("mouseenter", handleMouseEnter);
+// title.addEventListener("mouseleave", handleMouseLeave);
+
+const title = document.querySelector("div.Hello:first-child h1");
+
+function handleTitleClick(){
+    console.log("title was clicked !");
+    // title.style.color = "blue";
+}
+
+title.addEventListener("click", handleTitleClick);
