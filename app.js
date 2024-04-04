c34086155a01c551084ce2361ef62570ebb6e68e
hyojoo <jhj080802@naver.com>
2024-04-04 17:26:54 +0900

    event

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
-// const titke = document.querySelector("div.hello:first =<child h1");
-// function handleTitleClick(){
-//     handleTitleClick.style.color = "blue";
-// }
-
-// function handleMouseEnter(){
-//     handleTitleClick.innerText = "Mouse is here!";
-// }
-
-// function  handleMouseLeave(){
-//     handleTitleClick.innerText = "Mouse is gone!";
-// }
-
-// title.addEventListener("click", handleTitleClick);
-// title.addEventListener("mouseenter", handleMouseEnter);
-// title.addEventListener("mouseleave", handleMouseLeave);
-
-const title = document.querySelector("div.Hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    console.log("title was clicked !");
-    // title.style.color = "blue";
+    h1.style.color = "blue";
 }
 
-title.addEventListener("click", handleTitleClick);
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here!";
+}
+
+function  handleMouseLeave(){
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline(){
+    alert("ALL GOOD");
+}
+
+h1.addEventListener("click", handleTitleClick);
+//title.onclick = handleTitleClick; 
+h1.addEventListener("mouseenter", handleMouseEnter);
+//title.onmouseenter = handleMouseEnter;
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy",handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
