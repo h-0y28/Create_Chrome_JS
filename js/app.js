5da726afbeec1bf1c9bead3ec68a6a60afb6feca
hyojoo <jhj080802@naver.com>
2024-04-04 21:16:11 +0900

    onLoginBtnClick

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
 
-function handleTitleClick(){
-    h1.classList.toggle("clicked");
+function onLoginBtnClick() {
+    //console.log(loginInput.value);
+    //console.log("click!");
+
+    // const username = loginInput.value;
+    // if(username === ""){
+    //     alert("Please write your name.");
+    // } else if(username.length > 15){
+    //     alert("Your name is too long.");
+    // }
+
+    const username = loginInput.value;
+    console.log(username);
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginButton.addEventListener("click", onLoginBtnClick);
+
