ed1ec29c3f7dcc2130f0670667e4eca97378ab79
hyojoo <jhj080802@naver.com>
2024-04-08 20:30:20 +0900

    login+link

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginBtnClick(event) {
     event.preventDefault();
     console.log(loginInput.value);
 }
+function handleLinkClikck(event){
+        event.preventDefault(); //기본 동작 제어
+    console.dir(event)
+    alert("clicked!"); //기본 동작 제어, ok 누르면 기본 동작 실행
+}
 
 loginForm.addEventListener("submit", onLoginBtnClick);
+link.addEventListener("click",handleLinkClikck);
