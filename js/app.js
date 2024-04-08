117a86b0683fc2150a821a0188be33221acd62e2
hyojoo <jhj080802@naver.com>
2024-04-08 21:11:36 +0900

    fix Getting Username

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginBtnClick(event) {
     event.preventDefault();
-    console.log(loginInput.value);
-}
-function handleLinkClikck(event){
-        event.preventDefault(); //기본 동작 제어
-    console.dir(event)
-    alert("clicked!"); //기본 동작 제어, ok 누르면 기본 동작 실행
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = "Hello "+ username;
+    //greeting.innerText = `Hello ${username}`;
+
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginBtnClick);
-link.addEventListener("click",handleLinkClikck);
