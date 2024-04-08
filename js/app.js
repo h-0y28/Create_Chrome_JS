4e12acaeb997c1c3c966e8a588e21fef996100cc
hyojoo <jhj080802@naver.com>
2024-04-08 20:04:46 +0900

    login form

--- js/app.js
+++ js/app.js
@@ -1,21 +1,9 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-function onLoginBtnClick() {
-    //console.log(loginInput.value);
-    //console.log("click!");
-
-    // const username = loginInput.value;
-    // if(username === ""){
-    //     alert("Please write your name.");
-    // } else if(username.length > 15){
-    //     alert("Your name is too long.");
-    // }
-
-    const username = loginInput.value;
-    console.log(username);
+function onLoginBtnClick(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
-
+loginForm.addEventListener("submit", onLoginBtnClick);
