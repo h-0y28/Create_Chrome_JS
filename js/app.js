8324528f2fdd7cbeb2ebf37467ff6caff6077a6a
hyojoo <jhj080802@naver.com>
2024-04-14 16:25:51 +0900

    loading Username

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-
+const USERNAME_KEY = "username";
 const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginBtnClick(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    greeting.innerText = "Hello "+ username;
-    //greeting.innerText = `Hello ${username}`;
-
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginBtnClick);
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+const saveUsername = localStorage.getItem(USERNAME_KEY);
 
+console.log(saveUsername);
+
+if(saveUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginBtnClick);
+} else {
+    // ahoe the greetings
+    paintGreetings(saveUsername);
+}
