7e1baa6588a431b8efa7af9b2943b51584a822e8
hyojoo <jhj080802@naver.com>
2024-04-18 19:01:41 +0900

    Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,14 @@ const clock = document.querySelector("#clock");
 
 function getClock(){
     const clock = document.querySelector("h2#clock");
-    function sayHello(){
-        console.log("hello");
-    }
-    setInterval(sayHello, 5000);    
+
+function getClock(){
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+}
+
+getClock();
+setInterval(getClock, 1000);    
 }
 
 getClock();
