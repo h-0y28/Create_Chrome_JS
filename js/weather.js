3a86f0476bcbd09d17bea818e775db1684943605
hyojoo <jhj080802@naver.com>
2024-04-25 11:09:55 +0900

    #8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "968690e9627ec125bdb9570e305567ee";
+
 function onGeoOk(position){
-        const lat = position.coords.latitude;
-        const lng = position.coords.longitude;
-    console.log("You live it", lat,lng);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log("You live it", lat,lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang=ko&units=metric`
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>{
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError(){
     alert("can't find you. No weather for you. ");
